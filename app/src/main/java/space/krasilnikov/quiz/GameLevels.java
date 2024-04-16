0920c2af98f8302b7b27f08585ea9d10533b6e9f
betelgeyzUA <betelgeyzUA@gmail.com>
2024-04-16 19:51:13 +0300

    GameLevels

---
+++ app/src/main/java/space/krasilnikov/quiz/GameLevels.java
@@ -0,0 +1,24 @@
+package space.krasilnikov.quiz;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.gamelevels);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gamelevels), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/space/krasilnikov/quiz/MainActivity.java
+++ app/src/main/java/space/krasilnikov/quiz/MainActivity.java
@@ -1,6 +1,9 @@
 package space.krasilnikov.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, GameLevels.class);
+                    startActivity(intent);finish();
+                } catch (Exception e) {
+
+                } // конец конструкции
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
