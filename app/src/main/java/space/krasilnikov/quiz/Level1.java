941c105eb9c7343a8918c31cae3c5e7c7c70494a
betelgeyzUA <betelgeyzUA@gmail.com>
2024-04-18 13:26:43 +0300

    Level content

--- app/src/main/java/space/krasilnikov/quiz/Level1.java
+++ app/src/main/java/space/krasilnikov/quiz/Level1.java
@@ -2,6 +2,9 @@ package space.krasilnikov.quiz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,26 @@ public class Level1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.universal);
+
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Level1.this, GameLevels.class);
+                    startActivity(intent); finish();
+                } catch (Exception e) {
+
+                }
+            }
+        });
+
+        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        img_left.setClipToOutline(true);
+
+        final ImageView img_right = (ImageView)findViewById(R.id.img_right);
+        img_right.setClipToOutline(true);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
