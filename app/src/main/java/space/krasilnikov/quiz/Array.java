cfa8659b0c4a908caf2d7cf6842b172fe42f58f2
betelgeyzUA <betelgeyzUA@gmail.com>
2024-04-20 21:28:46 +0300

    Add array levels

---
+++ app/src/main/java/space/krasilnikov/quiz/Array.java
@@ -0,0 +1,32 @@
+package space.krasilnikov.quiz;
+
+public class Array
+{
+    final int[] images1 = {
+            R.drawable.one_level_zero,
+            R.drawable.one_level_one,
+            R.drawable.one_level_two,
+            R.drawable.one_level_thee,
+            R.drawable.one_level_four,
+            R.drawable.one_level_five,
+            R.drawable.one_level_six,
+            R.drawable.one_level_seven,
+            R.drawable.one_level_eight,
+            R.drawable.one_level_nine,
+    };
+
+    final int[] power = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    final int[] texts1 = {
+            R.string.lvl1text0,
+            R.string.lvl1text1,
+            R.string.lvl1text2,
+            R.string.lvl1text3,
+            R.string.lvl1text4,
+            R.string.lvl1text5,
+            R.string.lvl1text6,
+            R.string.lvl1text7,
+            R.string.lvl1text8,
+            R.string.lvl1text9
+    };
+}
